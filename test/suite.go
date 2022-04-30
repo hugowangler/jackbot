3369fd4849eadcbb97fc89a0dcc54cd1a3d6543f
Måns Falk <mansfalk@msn.com>
2022-05-01 01:22:31 +0200

    attempt nr 2 to fix build errors

--- test/suite.go
+++ test/suite.go
@@ -43,6 +43,6 @@ func RunMigrations(db *gorm.DB) error {
 }
 
 func CleanTestDb(db *gorm.DB) error {
-	res := db.Exec("DELETE FROM rows;DELETE FROM raffles;DELETE FROM users;DELETE FROM games;DELETE FROM permissions")
+	res := db.Exec("DELETE FROM rows;DELETE FROM raffles;DELETE FROM games;DELETE FROM users;DELETE FROM permissions")
 	return res.Error
 }
