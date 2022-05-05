96cbe1c96134cfcfd19e0f4d9dd8ed296b632bcb
Måns Falk <mansfalk@msn.com>
2022-05-05 23:11:04 +0200

    fix tests

--- internal/bot/commands_test.go
+++ internal/bot/commands_test.go
@@ -19,7 +19,7 @@ import (
 
 const okResponse = "game created: jacken"
 const validCreateGame = "!creategame --name=jacken --numbers=5 --numbersrange=50 --bonusnumbers=2 --bonusrange=12 " +
-	"--entryfee=5"
+	"--entryfee=5 --accountant=abc123"
 
 type CommandsTS struct {
 	suite.Suite
@@ -93,73 +93,79 @@ func (s *CommandsTS) TestCommands_HandleInput_CreateGame() {
 	}{
 		{
 			name:     "valid",
-			input:    "!creategame --name=jacken --numbers=5 --numbersrange=50 --bonusnumbers=2 --bonusrange=12 --entryfee=5",
+			input:    "!creategame --name=jacken --numbers=5 --numbersrange=50 --bonusnumbers=2 --bonusrange=12 --entryfee=5 --accountant=abc123",
 			exp:      okResponse,
 			authorId: test.MockUser.Id,
 		},
 		{
 			name:     "missing name",
-			input:    "!creategame --numbers=5 --numbersrange=50 --bonusnumbers=2 --bonusrange=12 --entryfee=5",
+			input:    "!creategame --numbers=5 --numbersrange=50 --bonusnumbers=2 --bonusrange=12 --entryfee=5 --accountant=abc123",
 			exp:      "name is required",
 			authorId: test.MockUser.Id,
 		},
 		{
 			name:     "missing numbers",
-			input:    "!creategame --name=jacken --numbersrange=50 --bonusnumbers=2 --bonusrange=12 --entryfee=5",
+			input:    "!creategame --name=jacken --numbersrange=50 --bonusnumbers=2 --bonusrange=12 --entryfee=5 --accountant=abc123",
 			exp:      "numbers is required",
 			authorId: test.MockUser.Id,
 		},
 		{
 			name:     "missing numbersrange",
-			input:    "!creategame --name=jacken --numbers=5 --bonusnumbers=2 --bonusrange=12 --entryfee=5",
+			input:    "!creategame --name=jacken --numbers=5 --bonusnumbers=2 --bonusrange=12 --entryfee=5 --accountant=abc123",
 			exp:      "numbersrange is required",
 			authorId: test.MockUser.Id,
 		},
 		{
 			name:     "missing bonusnumbers",
-			input:    "!creategame --name=jacken --numbers=5 --numbersrange=50 --bonusrange=12 --entryfee=5",
+			input:    "!creategame --name=jacken --numbers=5 --numbersrange=50 --bonusrange=12 --entryfee=5 --accountant=abc123",
 			exp:      "bonusnumbers is required",
 			authorId: test.MockUser.Id,
 		},
 		{
 			name:     "missing bonusrange",
-			input:    "!creategame --name=jacken --numbers=5 --numbersrange=50 --bonusnumbers=2 --entryfee=5",
+			input:    "!creategame --name=jacken --numbers=5 --numbersrange=50 --bonusnumbers=2 --entryfee=5 --accountant=abc123",
 			exp:      "bonusrange is required",
 			authorId: test.MockUser.Id,
 		},
 		{
 			name:     "missing entryfee",
-			input:    "!creategame --name=jacken --numbers=5 --numbersrange=50 --bonusnumbers=2 --bonusrange=12",
+			input:    "!creategame --name=jacken --numbers=5 --numbersrange=50 --bonusnumbers=2 --bonusrange=12 --accountant=abc123",
 			exp:      "entryfee is required",
 			authorId: test.MockUser.Id,
 		},
 		{
+			name:     "missing accountant",
+			input:    "!creategame --name=jacken --numbers=5 --numbersrange=50 --bonusnumbers=2 --bonusrange=12 --entryfee=5",
+			exp:      "accountant is required",
+			authorId: test.MockUser.Id,
+		},
+		{
 			name:     "numbers not an integer",
-			input:    "!creategame --name=jacken --numbers=a --numbersrange=50 --bonusnumbers=2 --bonusrange=12 --entryfee=5",
+			input:    "!creategame --name=jacken --numbers=a --numbersrange=50 --bonusnumbers=2 --bonusrange=12 --entryfee=5 --accountant=abc123",
 			exp:      "numbers must be an integer",
 			authorId: test.MockUser.Id,
 		},
 		{
 			name:     "numbersrange not an integer",
-			input:    "!creategame --name=jacken --numbers=5 --numbersrange=b --bonusnumbers=2 --bonusrange=12 --entryfee=5",
+			input:    "!creategame --name=jacken --numbers=5 --numbersrange=b --bonusnumbers=2 --bonusrange=12 --entryfee=5 --accountant=abc123",
 			exp:      "numbersrange must be an integer",
 			authorId: test.MockUser.Id,
 		},
 		{
 			name:     "bonusnumbers not an integer",
-			input:    "!creategame  --name=jacken --numbers=5 --numbersrange=50 --bonusnumbers=a --bonusrange=12 --entryfee=5",
+			input:    "!creategame  --name=jacken --numbers=5 --numbersrange=50 --bonusnumbers=a --bonusrange=12 --entryfee=5 --accountant=abc123",
 			exp:      "bonusnumbers must be an integer",
 			authorId: test.MockUser.Id,
 		},
 		{
 			name:     "bonusrange not an integer",
-			input:    "!creategame --name=jacken --numbers=5 --numbersrange=50 --bonusnumbers=2 --bonusrange=e --entryfee=5",
+			input:    "!creategame --name=jacken --numbers=5 --numbersrange=50 --bonusnumbers=2 --bonusrange=e --entryfee=5 --accountant=abc123",
 			exp:      "bonusrange must be an integer",
 			authorId: test.MockUser.Id,
 		},
 		{
 			name:     "entryfee not an integer",
-			input:    "!creategame --name=jacken --numbers=5 --numbersrange=50 --bonusnumbers=2 --bonusrange=12 --entryfee=a",
+			input:    "!creategame --name=jacken --numbers=5 --numbersrange=50 --bonusnumbers=2 --bonusrange=12 --entryfee=a --accountant=abc123",
 			exp:      "entryfee must be an integer",
 			authorId: test.MockUser.Id,
 		},
